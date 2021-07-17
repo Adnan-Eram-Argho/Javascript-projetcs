0c5054383b37fc7333f6c778f2f707d21293430a
Adnan-Eram-Argho <83761178+Adnan-Eram-Argho@users.noreply.github.com>
2021-07-17 20:08:35 +0600

    sdjk

--- Bmi calculator/javascript.js
+++ Bmi calculator/javascript.js
@@ -1,10 +1,13 @@
-const feet = document.getElementById("feet");
-const feetValue = parseFloat(feet.value);
-const  kg = document.getElementById("kg")
-const kgValue =parseFloat(kg.value);
-const result = document.getElementById("result");
+
 
 function result2(){
+    const feet = document.getElementById("feet");
+    const feetValue = parseFloat(feet.value);
+    const  kg = document.getElementById("kg")
+    const kgValue =parseFloat(kg.value);
+    const result = document.getElementById("result");
+
+
     var result1= (kgValue/ ((feetValue/3.281)*(feetValue/3.281))).toFixed('4');
     result.innerHTML = result1  ;
     
