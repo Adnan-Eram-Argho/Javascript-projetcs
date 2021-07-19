87c9decba1f37466d5751f240d1d5d19a9da0dcd
Adnan-Eram-Argho <83761178+Adnan-Eram-Argho@users.noreply.github.com>
2021-07-19 22:39:47 +0600

    theme changer

---
+++ color switcher/javascript.js
@@ -0,0 +1,23 @@
+
+// document.getElementById("whiteButton").onclick = ;
+// document.getElementById("blueButton").onclick ="" ;
+// document.getElementById("yellowButton").onclick ="" ;
+
+// function switchGray(){
+//     document.getElementsByTagName("body")[0].style.backgroundColor = 'gray';
+// }
+// function switchblue(){
+//     document.getElementsByTagName("body")[0].style.backgroundColor = 'white';
+// }
+// function switchBlue(){
+//     document.getElementsByTagName("body")[0].style.backgroundColor = 'blue';
+// }
+// function switchyellow(){
+//     document.getElementsByTagName("body")[0].style.backgroundColor = 'yellow';
+// }
+
+
+
+function switchcolor(color){
+    document.getElementsByTagName("body")[0].style.backgroundColor = color;
+}
